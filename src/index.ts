64314b4b52d8bddc9a7b61426321770531b541c4
Milad Nekofar <milad@nekofar.com>
2024-02-04 09:13:28 +0330

    refactor: enhance code with best practices and optimizations

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,7 @@ import { html } from 'hono/html'
 const app = new Hono()
 
 app.get('/', (c) => {
-  const imageText = 'Hello World'
-  const frameImage = `https://placehold.co/600x400?text=${imageText}`
+  const frameImage = `https://placehold.co/600x400?text=Hello+World`
   const framePostUrl = c.req.url
 
   return c.html(html`
@@ -30,34 +29,37 @@ app.get('/', (c) => {
 })
 
 app.post('/', async (c) => {
-  const body = await c.req.json()
-  const {
-    untrustedData: { buttonIndex, inputText },
-  } = body
+  try {
+    const body = await c.req.json()
+    const { buttonIndex, inputText } = body.untrustedData
 
-  const backgroundColors = [undefined, 'green', 'purple', 'red', 'blue']
+    const backgroundColors = ['green', 'purple', 'red', 'blue']
 
-  const imageText = inputText || 'Hello World'
-  const imageColor = backgroundColors[buttonIndex]
+    const imageText = encodeURIComponent(inputText || 'Hello World')
+    const imageColor = backgroundColors[buttonIndex - 1] || 'white'
 
-  const frameImage = `https://placehold.co/600x400/${imageColor}/white?text=${imageText}`
-  const framePostUrl = c.req.url
+    const frameImage = `https://placehold.co/600x400/${imageColor}/white?text=${imageText}`
+    const framePostUrl = c.req.url
 
-  return c.html(html`
-    <html lang="en">
-      <head>
-        <meta property="fc:frame" content="vNext" />
-        <meta property="fc:frame:image" content="${frameImage}" />
-        <meta property="fc:frame:post_url" content="${framePostUrl}" />
-        <meta property="fc:frame:input:text" content="Enter a message" />
-        <meta property="fc:frame:button:1" content="Green" />
-        <meta property="fc:frame:button:2" content="Purple" />
-        <meta property="fc:frame:button:3" content="Red" />
-        <meta property="fc:frame:button:4" content="Blue" />
-        <title>Farcaster Frames</title>
-      </head>
-    </html>
-  `)
+    return c.html(html`
+      <html lang="en">
+        <head>
+          <meta property="fc:frame" content="vNext" />
+          <meta property="fc:frame:image" content="${frameImage}" />
+          <meta property="fc:frame:post_url" content="${framePostUrl}" />
+          <meta property="fc:frame:input:text" content="Enter a message" />
+          <meta property="fc:frame:button:1" content="Green" />
+          <meta property="fc:frame:button:2" content="Purple" />
+          <meta property="fc:frame:button:3" content="Red" />
+          <meta property="fc:frame:button:4" content="Blue" />
+          <title>Farcaster Frames</title>
+        </head>
+      </html>
+    `)
+  } catch (error) {
+    console.error(error)
+    return c.json({ error: 'Invalid request' }, 400)
+  }
 })
 
 const port = 3000
